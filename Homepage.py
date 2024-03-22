af507f5e3b59e026349d9d1d3553135b26854876
guoning <357027361@qq.com>
2024-03-22 17:29:32 +0800

    流式输出

--- Homepage.py
+++ Homepage.py
@@ -14,7 +14,7 @@ if "OPENAI_API_KEY" not in st.session_state:
 elif st.session_state["OPENAI_API_KEY"] != "":
     chat = ChatOpenAI(openai_api_key=st.session_state["OPENAI_API_KEY"])
 
-st.set_page_config(page_title="Welcome to ASL", layout="wide")
+st.set_page_config(page_title="哇卡玛咖", layout="wide")
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [SystemMessage(content="你是无所不能的罗伯特，请问我任何你想提问的问题")]
@@ -44,16 +44,16 @@ def ask(prompt):
 
 
 def main():
-    with st.container(height=600):
+    with st.container(height=800, border=False):
         show_messages()
-        assistant = st.empty()
+        user = st.empty()
         ai = st.empty()
 
-    with st.container(height=80):
+    with st.container(height=80, border=False):
         prompt = st.chat_input("Type something...")
         if prompt:
             # 更新空元素中的内容，实现流式呈现
-            assistant.chat_message('user').write(prompt)
+            user.chat_message('user').write(prompt)
             ai.chat_message('assistant').write("思考中....")
             ai.chat_message('assistant').write_stream(ask(prompt))
 
