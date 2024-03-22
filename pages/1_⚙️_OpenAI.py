910d82df914a3b07b2bd4304b8c3ec2562c89ccf
guoning <357027361@qq.com>
2024-03-22 17:58:09 +0800

    流式输出

---
+++ pages/1_⚙️_OpenAI.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+if "OPENAI_API_KEY" not in st.session_state:
+    st.session_state["OPENAI_API_KEY"] = ""
+
+st.set_page_config(page_title="OpenAI Settings", layout="wide")
+
+st.title("OpenAI Settings")
+
+
+openai_api_key = st.text_input("API Key", value=st.session_state["OPENAI_API_KEY"], max_chars=None, key=None, type='password')
+
+saved = st.button("Save")
+
+if saved:
+    st.session_state["OPENAI_API_KEY"] = openai_api_key
+    # 当用户点击按钮时，将数据保存到LocalStorage中
+    st.write('数据已保存到LocalStorage')
+    st.write(f'用户输入的数据为: {openai_api_key}')
+
+    # 执行JavaScript代码将数据保存到LocalStorage
+    st.write(f'<script>localStorage.setItem("OPENAI_API_KEY", "{openai_api_key}")</script>')
+
+    # 在页面上显示从LocalStorage中读取的数据
+    stored_data = st.write('<script>localStorage.getItem("OPENAI_API_KEY")</script>')
+    st.write(stored_data)
